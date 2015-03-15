64c24242309989aacd4846a9ed3c2375cd1007ef
Samuel Atkins <sam@samatkins.co.uk>
2015-03-15 21:03:16 +0000

    Everything sucks and is confusing. Maybe SFML isn't for me, if I can't even get it to include properly.

--- src/build.bat
+++ src/build.bat
@@ -3,5 +3,5 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
 
 pushd ..\build
-cl ..\src\main.cpp /DSFML_STATIC=1 /FC /Zi /I ..\include /Zl /link ..\lib\sfml-system-s.lib
+cl ..\src\main.cpp -EHsc -FC -Zi -MT -DSFML_STATIC -I ..\include -link ..\lib\sfml-graphics-s.lib ..\lib\sfml-window-s.lib ..\lib\sfml-system-s.lib
 popd
