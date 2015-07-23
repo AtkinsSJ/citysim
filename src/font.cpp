88a9de3a947310c07a4d91ab195bcbf67f5575b6
Samuel Atkins <sam@samatkins.co.uk>
2015-07-23 15:51:53 +0100

    Got  a 'Hello World!' tooltip to work, yay! :D

--- src/font.cpp
+++ src/font.cpp
@@ -1,6 +1,34 @@
 // font.cpp
 
+BitmapFontChar *findChar(BitmapFont *font, uint32 uChar)
+{
+	// TODO: Binary search!
+
+	BitmapFontChar *result = 0;
+
+	for (uint32 i=0;
+		i < font->charCount;
+		i++)
+	{
+		if (font->chars[i].id == uChar) {
+			result = font->chars + i;
+			break;
+		}
+	}
+
+	return result;
+}
+
 void drawText(GLRenderer *renderer, BitmapFont *font, V2 position, char *text)
 {
-	
+	for (char *currentChar=text;
+		*currentChar != 0;
+		currentChar++)
+	{
+		uint32 uChar = (uint32)(*currentChar);
+		BitmapFontChar *c = findChar(font, uChar);
+		drawQuad(renderer, true, rectXYWH(position.x + c->xOffset, position.y + c->yOffset, c->size.w, c->size.h),
+					0, c->textureID, c->uv, 0);
+		position.x += c->xAdvance;
+	}
 }
--- src/main.cpp
+++ src/main.cpp
@@ -928,7 +928,7 @@ int main(int argc, char *argv[]) {
 			}
 		}
 
-		drawText(renderer, renderer->theme.font, v2(mouseState.pos), "Hello world!");
+		drawText(renderer, renderer->theme.font, v2(mouseState.pos) + tooltip.offsetFromCursor, "Hello world!");
 
 		// GAME OVER
 		if (gameStatus == GameStatus_Lost
