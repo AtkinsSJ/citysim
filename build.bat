46163ac70775e8ee3bab3cc86909d4c9ac16b89c
AtkinsSJ <git@samatkins.co.uk>
2018-12-18 16:43:58 +0000

    Batch files are in the root now, where they should be.

--- build.bat
+++ build.bat
@@ -10,6 +10,6 @@ rem set COMPILER_FLAGS= -EHsc -MD -W4 -wd4201 -wd4996 -Zi -Od -DBUILD_DEBUG=1 -I
 set COMPILER_FLAGS= -EHsc -MD -W4 -wd4201 -wd4996 -Zi -Od -DBUILD_DEBUG=1 -I ..\include -FC
 set LINKER_FLAGS= -SUBSYSTEM:WINDOWS -LIBPATH:..\lib\x86 -debug SDL2_image.lib SDL2main.lib SDL2.lib Shell32.lib opengl32.lib glew32.lib glu32.lib
 
-pushd ..\build
+pushd build
 cl ..\src\main.cpp %COMPILER_FLAGS% -link %LINKER_FLAGS%
 popd
--- cloc.bat
+++ cloc.bat
@@ -2,4 +2,4 @@
 rem c:\tools\cloc app/src/ buildsrc/src/ aFileChooser/src/ svg/ data/
 rem c:\tools\cloc --vcs=git --by-file-by-lang
 @echo on
-c:\tools\cloc --by-file-by-lang --skip-uniqueness . ../build/assets/
+c:\tools\cloc --by-file-by-lang --skip-uniqueness src/ build/assets/
