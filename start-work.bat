5254ec57a66b8392aea49b71e45f2f8dfb328bee
AtkinsSJ <git@samatkins.co.uk>
2021-04-05 14:31:25 +0100

    Note

--- start-work.bat
+++ start-work.bat
@@ -3,6 +3,7 @@ REM this is a copy of the batch file I use to start working on this.
 
 explorer P:\citysim
 cd P:\citysim
+REM This is VS2017
 start .\build\citysim.sln
 start "" "C:\Program Files\Sublime Text 3\sublime_text.exe" -p P:\citysim\citysim.sublime-project
 
@@ -10,4 +11,3 @@ start "C:\Program Files\PuTTY\pageant.exe" C:\Users\msn\Documents\ssh\privatekey
 start "" "C:\Program Files\Sublime Merge\sublime_merge.exe"
 
 wt new-tab -p "C++ Dev" -d P:\citysim --title CitySim
-REM cmd.exe /k "call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64"
