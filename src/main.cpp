6e39f6339010243436e65be35b0901f694bfd04c
AtkinsSJ <git@samatkins.co.uk>
2021-04-05 15:34:19 +0100

    Combine window update and render passes into updateAndRenderWindows()
    It's buggy, but I know why, and it's a good starting point.
    
    CLOC: 22858

--- src/main.cpp
+++ src/main.cpp
@@ -356,7 +356,8 @@ int main(int argc, char *argv[])
 			{
 				uiState.uiRects.clear();
 				uiState.mouseInputHandled = false;
-				updateWindows(&uiState);
+				// updateWindows(&uiState);
+				updateAndRenderWindows(&uiState);
 				
 				AppStatus newAppStatus = appState->appStatus;
 
@@ -387,7 +388,7 @@ int main(int argc, char *argv[])
 					INVALID_DEFAULT_CASE;
 				}
 
-				renderWindows(&uiState);
+				// renderWindows(&uiState);
 
 				if (newAppStatus != appState->appStatus)
 				{
