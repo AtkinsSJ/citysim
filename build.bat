a7a7136a93ad977c35f44df5bb9e8dfdd00c518d
AtkinsSJ <git@samatkins.co.uk>
2021-06-16 13:03:38 +0100

    Specify C++ standard in build command
    Wanted to use c++20 for struct initializers, but my VS version
    (2017) doesn't support that. :(

--- build.bat
+++ build.bat
@@ -6,7 +6,7 @@ where /q cl.exe || call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Com
 
 REM to send preprocessor output to a file, add this to the compiler flags:  -P -Fipreprocessed.output 
 
-set COMPILER_FLAGS= -EHsc -MD -W4 -wd4201 -Zi -Od -Oi -DBUILD_DEBUG=1 -I ..\include -FC 
+set COMPILER_FLAGS= -EHsc -MD -W4 -wd4201 -Zi -Od -Oi -std:c++17 -DBUILD_DEBUG=1 -I ..\include -FC 
 set LINKER_FLAGS= -SUBSYSTEM:WINDOWS -LIBPATH:..\lib\x64 -debug SDL2_image.lib SDL2main.lib SDL2.lib Shell32.lib opengl32.lib glew32.lib glu32.lib
 
 pushd build
