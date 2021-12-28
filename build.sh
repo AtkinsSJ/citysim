fdbc426a6dfb2b0a2dfb0cbf57cf549f9ac066c6
Sam Atkins <atkinssj@gmail.com>
2021-12-28 20:02:26 +0000

    Enable all warnings (except unknown-pragmas)
    Unknown-pragmas are a necessity if we want to continue supporting MSVC
    as a build environment. Which right now, we do! Maybe later I won't
    bother.

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 clang++ ./src/main.cpp -o build/citysim \
 	-std=c++20 --debug \
+	-Wall -Werror -Wno-unknown-pragmas \
 	-DBUILD_DEBUG=1 \
 	-I ./include \
 	-lSDL2 -lSDL2main -lSDL2_image -lGLEW -lGLU -lGL
-
-#	-Wall -Werror \
