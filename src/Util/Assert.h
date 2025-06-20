1d1e697df138b882ba7cc91f67a486afe5c4acef
Sam Atkins <atkinssj@gmail.com>
2025-06-20 20:13:50 +0100

    Meta: Set up compiler flags in cmake build

--- src/Util/Assert.h
+++ src/Util/Assert.h
@@ -14,21 +14,9 @@
 // So, I've added what SHOULD be a no-op wrapper around it, using the (a,b) comma trick.
 // (Well, it's not a no-op in debug builds, but the release optimiser does make it a no-op!)
 // - Sam, 10/07/2019
-#define ASSERT(expr)      \
-    if (1, true) {        \
-        SDL_assert(expr); \
-    } else {              \
-    }
-#define ASSERT_PARANOID(expr)      \
-    if (1, true) {                 \
-        SDL_assert_paranoid(expr); \
-    } else {                       \
-    }
-#define ASSERT_RELEASE(expr)      \
-    if (1, true) {                \
-        SDL_assert_release(expr); \
-    } else {                      \
-    }
+#define ASSERT(expr) SDL_assert(expr)
+#define ASSERT_PARANOID(expr) SDL_assert_paranoid(expr)
+#define ASSERT_RELEASE(expr) SDL_assert_release(expr)
 
 #define INVALID_DEFAULT_CASE \
     default:                 \
