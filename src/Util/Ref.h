d163fad9f6211aa62846750639e432fb7b3259a8
Sam Atkins <atkinssj@gmail.com>
2025-08-10 14:46:52 +0100

    Util: Add a basic Ref type for references
    C++ doesn't like putting references into unions, which includes our
    Optional type. So to get around that, this is a custom pointer type
    that is not allowed to be null.
    
    Probably it's missing some things, but I can add those as required.

---
+++ src/Util/Ref.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2025, Sam Atkins <sam@samatkins.co.uk>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+template<typename T>
+class Ref {
+public:
+    Ref() = delete;
+
+    Ref(T& value)
+        : m_value(&value)
+    {
+    }
+
+    operator T&() const { return *m_value; }
+    T* operator->() const { return m_value; }
+    T& operator*() const { return *m_value; }
+
+private:
+    T* m_value;
+};
