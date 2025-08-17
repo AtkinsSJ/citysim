41292f24575c4f64d215f74f911204df4eaad5ad
Sam Atkins <atkinssj@gmail.com>
2025-08-17 17:45:23 +0100

    Util: Replace internal uses of makeString() and remove it

--- src/Util/String.cpp
+++ src/Util/String.cpp
@@ -28,29 +28,6 @@ String::String(char const* chars, size_t length, WithHash with_hash)
         hashString(this);
 }
 
-String makeString(char* chars, s32 length, bool hash)
-{
-    String result = {};
-    result.chars = chars;
-    result.length = length;
-    result.hash = 0;
-
-    if (hash) {
-        hashString(&result); // NB: Sets the hash and hasHash.
-    }
-
-    return result;
-}
-String makeString(char* chars, bool hash)
-{
-    return makeString(chars, truncate32(strlen(chars)), hash);
-}
-// const is a huge pain in the bum
-String makeString(char const* chars, bool hash)
-{
-    return makeString((char*)chars, truncate32(strlen(chars)), hash);
-}
-
 Optional<String> String::from_blob(Blob const& blob, WithHash with_hash)
 {
     String result { (char*)blob.data(), (u32)truncate32(blob.size()) };
@@ -359,7 +336,8 @@ String String::next_token(String* remainder, Optional<char> split_char) const
         // NB: We have to make sure we properly initialise remainder here, because we had a bug before
         // where we didn't, and it sometimes had old data in the "hasHash" field, which was causing all
         // kinds of weird stuff to happen!
-        *remainder = makeString(first_word.chars + first_word.length, length - first_word.length).trimmed(TrimSide::Start);
+        *remainder = String { first_word.chars + first_word.length, (size_t)(length - first_word.length) }
+                         .trimmed(TrimSide::Start);
 
         // Skip the split char
         if (split_char.has_value() && remainder->length > 0) {
@@ -378,18 +356,18 @@ bool splitInTwo(String input, char divider, String* leftResult, String* rightRes
 
     bool foundDivider = false;
 
-    for (s32 i = 0; i < input.length; i++) {
+    for (auto i = 0u; i < input.length; i++) {
         if (input.chars[i] == divider) {
             // NB: We have to make sure we properly initialise leftResult/rightResult here, because we had a
             // bug before where we didn't, and it sometimes had old data in the "hasHash" field, which was
             // causing all kinds of weird stuff to happen!
             // Literally the exact same issue I fixed in nextToken() yesterday. /fp
             if (leftResult != nullptr) {
-                *leftResult = makeString(input.chars, i);
+                *leftResult = String { input.chars, (size_t)i };
             }
 
             if (rightResult != nullptr) {
-                *rightResult = makeString(input.chars + i + 1, input.length - i - 1);
+                *rightResult = String { input.chars + i + 1, (size_t)(input.length - i - 1) };
             }
 
             foundDivider = true;
@@ -468,7 +446,7 @@ String myprintf(String format, std::initializer_list<String> args, bool zeroTerm
 
             if (indexLength > 0) {
                 // Positional
-                String index_string = makeString(format.chars + startOfNumber, indexLength);
+                String index_string { format.chars + startOfNumber, (size_t)indexLength };
                 if (auto parsed_index = index_string.to_int(); parsed_index.has_value())
                     index = (s32)parsed_index.value();
             }
@@ -514,7 +492,7 @@ String formatInt(u64 value, u8 base, s32 zeroPadWidth)
     ASSERT((base > 1) && (base <= 36)); // formatInt() only handles base 2 to base 36.
     s32 arraySize = max(64, zeroPadWidth);
     char* temp = temp_arena().allocate_multiple<char>(arraySize); // Worst case is base 1, which is 64 characters!
-    s32 count = 0;
+    auto count = 0u;
 
     u64 v = value;
 
@@ -528,7 +506,7 @@ String formatInt(u64 value, u8 base, s32 zeroPadWidth)
         temp[arraySize - 1 - count++] = '0';
     }
 
-    return makeString(temp + (arraySize - count), count);
+    return String { temp + (arraySize - count), count };
 }
 
 String formatInt(s64 value, u8 base, s32 zeroPadWidth)
@@ -537,7 +515,7 @@ String formatInt(s64 value, u8 base, s32 zeroPadWidth)
     s32 arraySize = max(65, zeroPadWidth + 1);
     char* temp = temp_arena().allocate_multiple<char>(arraySize); // Worst case is base 1, which is 64 characters! Plus 1 for sign
     bool isNegative = (value < 0);
-    s32 count = 0;
+    auto count = 0u;
 
     // One complication here: If we're passed s64_MIN, then -value is 1 larger than can be held in an s64!
     // So, rather than flipping it and treating it like a positive number with an '-' appended,
@@ -559,7 +537,7 @@ String formatInt(s64 value, u8 base, s32 zeroPadWidth)
         temp[arraySize - 1 - count++] = '-';
     }
 
-    return makeString(temp + (arraySize - count), count);
+    return String { temp + (arraySize - count), count };
 }
 
 // TODO: formatFloat() is a total trainwreck, we should really do this a lot better!
@@ -570,11 +548,11 @@ String formatFloat(double value, s32 decimalPlaces)
 {
     String formatString = myprintf("%.{0}f"_s, { formatInt(decimalPlaces) }, true);
 
-    s32 length = 100; // TODO: is 100 enough?
+    size_t length = 100; // TODO: is 100 enough?
     char* buffer = temp_arena().allocate_multiple<char>(length);
-    s32 written = snprintf(buffer, length, formatString.chars, value);
+    size_t written = snprintf(buffer, length, formatString.chars, value);
 
-    return makeString(buffer, min(written, length));
+    return { buffer, min(written, length) };
 }
 
 String formatString(String value, s32 length, bool alignLeft, char paddingChar)
@@ -582,33 +560,31 @@ String formatString(String value, s32 length, bool alignLeft, char paddingChar)
     if ((value.length == length) || (length == -1))
         return value;
 
-    if (length < value.length) {
-        String result = makeString(value.chars, length);
-        return result;
-    } else {
-        String result = pushString(&temp_arena(), length);
-        result.length = length;
+    if (length < value.length)
+        return { value.chars, (size_t)length };
 
-        if (alignLeft) {
-            for (s32 i = 0; i < value.length; i++) {
-                result.chars[i] = value.chars[i];
-            }
-            for (s32 i = value.length; i < length; i++) {
-                result.chars[i] = paddingChar;
-            }
-        } else // alignRight
-        {
-            s32 startPos = length - value.length;
-            for (s32 i = 0; i < value.length; i++) {
-                result.chars[i + startPos] = value.chars[i];
-            }
-            for (s32 i = 0; i < startPos; i++) {
-                result.chars[i] = paddingChar;
-            }
+    String result = pushString(&temp_arena(), length);
+    result.length = length;
+
+    if (alignLeft) {
+        for (s32 i = 0; i < value.length; i++) {
+            result.chars[i] = value.chars[i];
         }
-
-        return result;
+        for (s32 i = value.length; i < length; i++) {
+            result.chars[i] = paddingChar;
+        }
+    } else // alignRight
+    {
+        s32 startPos = length - value.length;
+        for (s32 i = 0; i < value.length; i++) {
+            result.chars[i + startPos] = value.chars[i];
+        }
+        for (s32 i = 0; i < startPos; i++) {
+            result.chars[i] = paddingChar;
+        }
     }
+
+    return result;
 }
 
 String formatBool(bool value)
