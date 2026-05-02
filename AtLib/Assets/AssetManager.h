15904d30747b509ac1bf35eab63a3a478275d785
Sam Atkins <atkinssj@gmail.com>
2026-05-02 13:56:01 +0100

    Assets: Add missing HashTable include to AssetManager

--- AtLib/Assets/AssetManager.h
+++ AtLib/Assets/AssetManager.h
@@ -14,6 +14,7 @@
 #include <Settings/SettingsChangeListener.h>
 #include <Util/ChunkedArray.h>
 #include <Util/Function.h>
+#include <Util/HashTable.h>
 #include <Util/Set.h>
 #include <Util/StringTable.h>
 
