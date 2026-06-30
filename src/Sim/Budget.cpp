bbbbfd583a5f7da2ea203235379e899c615eb467
Sam Atkins <atkinssj@gmail.com>
2026-06-30 20:09:54 +0100

    Sim: Reimplement the demolition tool
    I'm struggling to figure out how to safely cache queries in variables,
    so I'm leaving that for a future change.

--- src/Sim/Budget.cpp
+++ src/Sim/Budget.cpp
@@ -6,7 +6,10 @@
 
 #include "Budget.h"
 
+#include <Sim/Game.h>
 #include <Sim/MapGeneration.h>
+#include <UI/UI.h>
+#include <UI/Window.h>
 
 mod_budget::mod_budget(flecs::world& world)
 {
@@ -38,4 +41,28 @@ bool Budget::can_afford(s32 cost) const
 void Budget::spend(s32 cost)
 {
     m_funds -= cost;
+}
+
+static void cost_tooltip_window_proc(UI::WindowContext* context, void* userData)
+{
+    auto* game_scene = dynamic_cast<GameScene*>(&App::the().scene());
+    if (!game_scene) {
+        context->closeRequested = true;
+        return;
+    }
+    auto& ui = context->windowPanel;
+    s32 cost = truncate32(reinterpret_cast<smm>(userData));
+
+    auto& budget = game_scene->world().get<Budget>();
+    auto style = budget.can_afford(cost)
+        ? "cost-affordable"_sv
+        : "cost-unaffordable"_sv;
+
+    String text = myprintf("£{0}"_s, { formatInt(cost) });
+    ui.addLabel(text, style);
+}
+
+void Budget::show_cost_tooltip(s32 cost) const
+{
+    UI::showTooltip(cost_tooltip_window_proc, reinterpret_cast<void*>(static_cast<smm>(cost)));
 }
