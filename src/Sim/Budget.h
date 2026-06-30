01afba9ebc204b32b13cbfd6abab42928c986181
Sam Atkins <atkinssj@gmail.com>
2026-06-30 19:48:10 +0100

    Sim: Implement a basic Budget type
    A place to store financial information, instead of the City struct. It's
    a class with some helpers.

---
+++ src/Sim/Budget.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2026, Sam Atkins <sam@samatkins.co.uk>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <Util/Basic.h>
+#include <flecs.h>
+
+struct mod_budget {
+    explicit mod_budget(flecs::world&);
+};
+
+class Budget {
+public:
+    explicit Budget(s32 funds);
+
+    bool can_afford(s32 cost) const;
+    void spend(s32 cost);
+
+    s32 funds() const { return m_funds; }
+    s32 monthly_expenditure() const { return m_monthly_expenditure; }
+
+private:
+    s32 m_funds { 0 };
+    s32 m_monthly_expenditure { 0 };
+};
